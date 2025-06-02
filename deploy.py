478040a94a6a783f934e327e530d3e52ee256690
Luis Bautista <39736480+lbautistab@users.noreply.github.com>
2025-06-02 12:51:14 -0500

    Add files via upload

---
+++ deploy.py
@@ -0,0 +1,29 @@
+from flask import Flask, render_template, request
+import pickle
+
+app = Flask(__name__)
+
+# Carga el modelo
+model = pickle.load(open('savedmodel.sav', 'rb'))
+
+@app.route('/')
+def home():
+    return render_template('index.html', result=None)
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    sepal_length = float(request.form['sepal_length'])
+    sepal_width = float(request.form['sepal_width'])
+    petal_length = float(request.form['petal_length'])
+    petal_width = float(request.form['petal_width'])
+    
+    data = [[sepal_length, sepal_width, petal_length, petal_width]]
+    prediction = model.predict(data)[0]
+    probabilities = model.predict_proba(data)[0]
+    clases = model.classes_
+    
+    # Crear lista con clases y probabilidades para pasar al template
+    probs = list(zip(clases, probabilities))
+    
+    return render_template('index.html', prediction=prediction, probs=probs)
+
